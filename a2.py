40115fcfc636033ab3218da948703a9ecd5897c6
Blake John Rowden <blakerowden@gmail.com>
2018-09-06 01:23:57 +1000

    Added Card Class
    Added the card class and began writing the basic template for it.

--- a2.py
+++ a2.py
@@ -9,7 +9,36 @@ import random
 
 __author__ = "Blake Rowden s4427634"
 
-# Write your classes here
+
+class Card(object):
+    def __init__(self, number, colour):
+        self._number = number
+        self._colour = colour
+
+    def get_number(self):
+        return self._number
+
+    def get_colour(self):
+        return self._colour
+
+    def set_number(self, number):
+        self._number = number
+
+    def set_colour(self, colour):
+        self._colour = colour
+
+    def get_pickup_amount(self):
+        pass  # Returns the amount of cards the next player should pickup
+
+    def matches(self, card):
+        if self.get_colour() == card.get_colour() or \
+           self.get_number() == card.get_number():
+            return True
+        else:
+            return False
+
+    def play(self, player, game):
+        pass  # Perform a special card action. The base Card class has no special action.
 
 
 def main():
